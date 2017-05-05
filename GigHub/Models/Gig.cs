582b091b0957d9766848c3526501f5dccfd3e9dc
learnbyexample <subashcic@gmail.com>
2017-05-04 22:51:03 -0400

    Create the Gigmodel

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        ApplicationUser Artist { get; set; }
+
+        public DateTimeOffset DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+    }
+}
