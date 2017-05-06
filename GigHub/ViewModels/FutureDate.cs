03c079e09568c6b62ed18ea80513a2c4cd71be80
learnbyexample <subashcic@gmail.com>
2017-05-06 14:08:24 -0400

    Added server side validation for gigs create form

---
+++ GigHub/ViewModels/FutureDate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModels
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime result;
+            return DateTime.TryParseExact(Convert.ToString(value),"d MMM yyyy",CultureInfo.CurrentCulture,DateTimeStyles.None,out result) && ( result < DateTime.Now);
+        }
+    }
+}
--- GigHub/ViewModels/GigViewModel.cs
+++ GigHub/ViewModels/GigViewModel.cs
@@ -1,23 +1,29 @@
 ﻿using GigHub.Models;
 using System.Collections.Generic;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GigHub.ViewModels
 {
     public class GigViewModel
     {
-
-
+       
+        [Required]
         public string Venue { get; set; }
+
+        [Required]
+        [FutureDate]
         public string Date { get; set; }
+
+        [Required]
+        [ValidTime]
         public string Time { get; set; }
+
+        [Required]
         public byte Genre { get; set; }
         public IEnumerable<Genre> Genres { get; set; }
-        public DateTime DateTime {
-            get
-            {
+        public DateTime GetDateTime() {
                 return DateTime.Parse($"{Date} {Time}");
-            }
         }
     }
 }
