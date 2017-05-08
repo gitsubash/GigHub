7f3bc3666118e252637badc9803fa1ff8fc20d43
learnbyexample <subashcic@gmail.com>
2017-05-07 23:41:01 -0400

    Add attendance webapi Add AttendanceDto Add ajax api call to create attendance

---
+++ GigHub/Controllers/api/AttendancesController.cs
@@ -0,0 +1,42 @@
+﻿using GigHub.DTOs;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/attendances")]
+    public class AttendancesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        //Web API don't look for scalar parameter from the body, it expects them from the url
+        //So, need to decorate our parameter with [FromBody]
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] AttendanceDto gigDto)  
+        {
+            var userId = User.Identity.GetUserId();
+
+            if ( _context.Attendances.Any(a => a.GigId == gigDto.GigId && a.AttendeeId == userId))
+            {
+                return BadRequest("The Attendance already exists");
+            }
+            
+            var attendance = new Attendance
+            {
+                GigId = gigDto.GigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ GigHub/DTOs/AttendanceDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.DTOs
+{
+    //Architectural pattern to send data across processes (using Data Transfer Objects concept)
+    //Communicate between two different pieces of code running in server and client
+    public class AttendanceDto
+    {
+        public int GigId { get; set; }
+    }
+}
